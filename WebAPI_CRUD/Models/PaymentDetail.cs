58faf375c936397eb65f1abd69c9a64c9e0fad23
nepodeveloper <neponed@gmail.com>
2021-05-14 11:19:28 +0200

    First Commit

--- WebAPI_CRUD/Models/PaymentDetail.cs
+++ WebAPI_CRUD/Models/PaymentDetail.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAPI_CRUD.Models
 {
-    public class PaymentDetails
+    public class PaymentDetail
     {
         [Key]
         public int PaymentDetailId { get; set; }
 
+        [Column(TypeName ="nvarchar(100)")]
         public string CardOwnerName { get; set; }
 
+        [Column(TypeName = "nvarchar(16)")]
         public string CardNumber { get; set; }
 
+        [Column(TypeName = "nvarchar(5)")]
         public string ExpirationDate { get; set; }
 
+        [Column(TypeName = "nvarchar(3)")]
         public string SecurityCode { get; set; }
     }
 }
--- WebAPI_CRUD/Startup.cs
+++ WebAPI_CRUD/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI_CRUD.Models;
 
 namespace WebAPI_CRUD
 {
@@ -31,6 +33,9 @@ namespace WebAPI_CRUD
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI_CRUD", Version = "v1" });
             });
+
+            services.AddDbContext<PaymentDetailContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

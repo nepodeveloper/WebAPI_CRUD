d593a696881591b26be8d971d780c833658b9273
nepodeveloper <neponed@gmail.com>
2021-05-14 10:20:09 +0200

    Add project files.

---
+++ WebAPI_CRUD/Models/PaymentDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_CRUD.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        public string CardOwnerName { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public string ExpirationDate { get; set; }
+
+        public string SecurityCode { get; set; }
+    }
+}
